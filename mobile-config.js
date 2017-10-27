687aeaa0206eff5f5e1365be35ad5e08135cb6ae
pbca26 <pbca26@gmail.com>
2017-10-27 18:50:47 +0300

    android ugly 9 patch splash

--- mobile-config.js
+++ mobile-config.js
@@ -13,13 +13,21 @@ App.info({
 
 // Set up resources such as icons and launch screens.
 App.icons({
-  'iphone_2x': 'icons/128x128.png',
-  'iphone_3x': 'icons/128x128.png',
-  'android_mdpi': 'icons/48x48.png',
-  'android_hdpi': 'icons/64x64.png',
-  'android_xhdpi': 'icons/96x96.png',
-  'android_xxhdpi': 'icons/128x128.png',
-  'android_xxxhdpi': 'icons/256x256.png',
+  'iphone_2x': 'app-assets/icons/128x128.png',
+  'iphone_3x': 'app-assets/icons/128x128.png',
+  'android_mdpi': 'app-assets/icons/48x48.png',
+  'android_hdpi': 'app-assets/icons/64x64.png',
+  'android_xhdpi': 'app-assets/icons/96x96.png',
+  'android_xxhdpi': 'app-assets/icons/128x128.png',
+  'android_xxxhdpi': 'app-assets/icons/256x256.png',
+});
+
+App.launchScreens({
+  'android_mdpi_portrait': 'app-assets/splash/drawable-mdpi/agama.9.png',
+  'android_hdpi_portrait': 'app-assets/splash/drawable-hdpi/agama.9.png',
+  'android_xhdpi_portrait': 'app-assets/splash/drawable-xhdpi/agama.9.png',
+  'android_xxhdpi_portrait': 'app-assets/splash/drawable-xxhdpi/agama.9.png',
+  'android_xxxhdpi_portrait': 'app-assets/splash/drawable-xxxhdpi/agama.9.png',
 });
 
 App.accessRule('*');
@@ -30,7 +38,7 @@ App.accessRule('*');
 });*/
 
 // Set PhoneGap/Cordova preferences.
-/*App.setPreference('BackgroundColor', '0xff0000ff');
-App.setPreference('HideKeyboardFormAccessoryBar', true);
+App.setPreference('BackgroundColor', '0xffffffff');
+/*App.setPreference('HideKeyboardFormAccessoryBar', true);
 App.setPreference('Orientation', 'default');
 App.setPreference('Orientation', 'all', 'ios');*/
