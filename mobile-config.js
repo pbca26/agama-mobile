d8688b5f75f10af2d4904ef8f2a1722be3429627
pbca26 <pbca26@gmail.com>
2017-11-05 20:30:12 +0300

    ios icons/splash

--- mobile-config.js
+++ mobile-config.js
@@ -11,8 +11,14 @@ App.info({
 
 // Set up resources such as icons and launch screens.
 App.icons({
-  'iphone_2x': 'app-assets/icons/128x128.png',
-  'iphone_3x': 'app-assets/icons/128x128.png',
+  // ios
+  'iphone': 'app-assets/icons/iphone.png',
+  'iphone_2x': 'app-assets/icons/iphone_2x.png',
+  'iphone_3x': 'app-assets/icons/iphone_3x.png',
+  'ipad': 'app-assets/icons/ipad.png',
+  'ipad_2x': 'app-assets/icons/ipad_2x.png',
+  'ipad_pro': 'app-assets/icons/ipad_pro.png',
+  // android
   'android_mdpi': 'app-assets/icons/48x48.png',
   'android_hdpi': 'app-assets/icons/64x64.png',
   'android_xhdpi': 'app-assets/icons/96x96.png',
@@ -21,6 +27,10 @@ App.icons({
 });
 
 App.launchScreens({
+  'iphone_2x': 'app-assets/splash/iphone_2x.png',
+  'iphone5': 'app-assets/splash/iphone5.png',
+  'iphone6': 'app-assets/splash/iphone6.png',
+  // android
   'android_mdpi_portrait': 'app-assets/splash/drawable-mdpi/agama.9.png',
   'android_hdpi_portrait': 'app-assets/splash/drawable-hdpi/agama.9.png',
   'android_xhdpi_portrait': 'app-assets/splash/drawable-xhdpi/agama.9.png',
@@ -28,6 +38,7 @@ App.launchScreens({
   'android_xxxhdpi_portrait': 'app-assets/splash/drawable-xxxhdpi/agama.9.png',
 });
 
+// App.accessRule('http://46.20.235.46');
 App.accessRule('*');
 
 /*App.launchScreens({
