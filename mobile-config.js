88a49e191fe0b9f6d76188977f2dab4d9943f818
pbca26 <pbca26@gmail.com>
2017-11-06 14:16:06 +0300

    app store icon; description update

--- mobile-config.js
+++ mobile-config.js
@@ -3,7 +3,7 @@ App.info({
   version: '0.0.3',
   buildNumber: '777',
   name: 'Agama',
-  description: 'Agama Wallet Mobile',
+  description: 'Agama Mobile is a lightweight wallet application that allows users to manage multiple crypto currencies. It\'s built on top of SPV technology which provides a quick way to retrieve and push transaction data.',
   author: 'SuperNET',
   email: 'dev@supernet.org',
   website: 'http://supernet.org'
@@ -18,6 +18,7 @@ App.icons({
   'ipad': 'app-assets/icons/ipad.png',
   'ipad_2x': 'app-assets/icons/ipad_2x.png',
   'ipad_pro': 'app-assets/icons/ipad_pro.png',
+  'app_store': 'app-assets/icons/app_store.png',
   // android
   'android_mdpi': 'app-assets/icons/48x48.png',
   'android_hdpi': 'app-assets/icons/64x64.png',
@@ -27,6 +28,7 @@ App.icons({
 });
 
 App.launchScreens({
+  // ios
   'iphone_2x': 'app-assets/splash/iphone_2x.png',
   'iphone5': 'app-assets/splash/iphone5.png',
   'iphone6': 'app-assets/splash/iphone6.png',
