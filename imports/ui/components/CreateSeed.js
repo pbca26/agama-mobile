c6497c1229404e260061b189207883e38cb2be48
pbca26 <pbca26@gmail.com>
2018-06-19 14:24:16 +0300

    new skin create seed

--- imports/ui/components/CreateSeed.js
+++ imports/ui/components/CreateSeed.js
@@ -93,85 +93,80 @@ class CreateSeed extends React.Component {
   render() {
     if (this.props.activeSection === 'create-seed') {
       return (
-        <div className="col-sm-12">
-          <div className="col-xlg-12 col-md-12 col-sm-12 col-xs-12 create-seed margin-bottom-40">
-            <div className="row">
-              <div className="margin-bottom-30">
-                <span
-                  className="btn-back"
-                  onClick={ () => this.props.changeActiveSection('login') }>
-                  <i className="fa fa-arrow-left"></i> { translate('DASHBOARD.BACK') }
-                </span>
-              </div>
-              <h4>{ translate('LOGIN.THIS_IS_YOUR_NEW_SEED') }</h4>
-              <div className="seed-gen-box">{ this.state.seed }</div>
-              <div className="text-center margin-top-30">
-                <QRCode
-                  value={ this.state.seed }
-                  size={ 320 } />
-              </div>
-              <div className="margin-top-25 margin-bottom-25 warning">
-                <strong>{ translate('LOGIN.PLEASE_MAKE_SURE_TO') }</strong>
-              </div>
-              <label className="switch">
-                <input
-                  type="checkbox"
-                  value="on"
-                  checked={ this.state.confirmSeedSaved } />
-                <div
-                  className="slider"
-                  onClick={ this.toggleConfirmSeed }></div>
-              </label>
+        <div className="form create-seed">
+          <h4>{ translate('LOGIN.THIS_IS_YOUR_NEW_SEED') }</h4>
+          <div className="seed-gen-box">{ this.state.seed }</div>
+          <div className="text-center margin-top-30">
+            <QRCode
+              value={ this.state.seed }
+              size={ 320 } />
+          </div>
+          <div className="margin-top-25 margin-bottom-25 text-center warning">
+            <strong>{ translate('LOGIN.PLEASE_MAKE_SURE_TO') }</strong>
+          </div>
+          <div className="margin-top-35 margin-bottom-35">
+            <label className="switch">
+              <input
+                type="checkbox"
+                value="on"
+                checked={ this.state.confirmSeedSaved } />
               <div
-                className="toggle-label pointer"
-                onClick={ this.toggleConfirmSeed }>
-                { translate('LOGIN.I_CONFIRM_I_SAVED_SEED') }
+                className="slider"
+                onClick={ this.toggleConfirmSeed }></div>
+            </label>
+            <div
+              className="toggle-label pointer"
+              onClick={ this.toggleConfirmSeed }>
+              { translate('LOGIN.I_CONFIRM_I_SAVED_SEED') }
+            </div>
+            { !this.state.confirmSeedSaved &&
+              !this.state.pristine &&
+              <div className="error margin-top-15">
+                <i className="fa fa-warning"></i> { translate('LOGIN.CONFIRMATION_REQUIRED') }
               </div>
-              { !this.state.confirmSeedSaved &&
-                !this.state.pristine &&
-                <div className="error margin-top-15">
-                  <i className="fa fa-warning"></i> { translate('LOGIN.CONFIRMATION_REQUIRED') }
-                </div>
-              }
-              <div className="margin-bottom-25 margin-top-30">
-                <label className="switch hide">
-                  <input
-                    type="checkbox"
-                    value="on"
-                    checked={ this.state.createPin } />
-                  <div
-                    className="slider"
-                    onClick={ this.toggleCreatePin }></div>
-                </label>
-                <div
-                  className="toggle-label pointer hide"
-                  onClick={ this.toggleCreatePin }>
-                  { translate('LOGIN.OVERRIDE_PIN') }
-                </div>
-                { this.state.createPin &&
-                  <input
-                    type="password"
-                    className="form-control margin-top-20"
-                    name="pinOverride"
-                    onChange={ this.updateInput }
-                    placeholder={ translate('LOGIN.ENTER_6_DIGIT_PIN') }
-                    value={ this.state.pinOverride || '' } />
-                }
-                { this.state.createPin &&
-                  this.state.pinOverrideTooShort &&
-                  <div className="error margin-top-15">
-                    <i className="fa fa-warning"></i> { translate('LOGIN.PIN_TOO_SHORT') }
-                  </div>
-                }
+            }
+          </div>
+          <div className="margin-bottom-25 margin-top-30">
+            <label className="switch hide">
+              <input
+                type="checkbox"
+                value="on"
+                checked={ this.state.createPin } />
+              <div
+                className="slider"
+                onClick={ this.toggleCreatePin }></div>
+            </label>
+            <div
+              className="toggle-label pointer hide"
+              onClick={ this.toggleCreatePin }>
+              { translate('LOGIN.OVERRIDE_PIN') }
+            </div>
+            { this.state.createPin &&
+              <div className="edit">
+                <input
+                  type="password"
+                  className="form-control"
+                  name="pinOverride"
+                  onChange={ this.updateInput }
+                  placeholder={ translate('LOGIN.ENTER_6_DIGIT_PIN') }
+                  value={ this.state.pinOverride || '' } />
               </div>
-
-              <button
-                className="btn btn-lg btn-primary btn-block ladda-button"
-                onClick={ this.login }>
-                <span className="ladda-label">
-                { translate('LOGIN.LOGIN') }
-                </span>
-              </button>
+            }
+            { this.state.createPin &&
+              this.state.pinOverrideTooShort &&
+              <div className="error margin-top-15">
+                <i className="fa fa-warning"></i> { translate('LOGIN.PIN_TOO_SHORT') }
+              </div>
+            }
+          </div>
+          <div
+            onClick={ this.login }
+            className="group3">
+            <div className="rectangle10copy"></div>
+            <div className="btn">Sign In</div>
+            <div className="group2">
+              <div className="rectangle8copy"></div>
+              <img className="path6" src="/images/template/login/reset-password-path-6.png" />
             </div>
           </div>
         </div>
