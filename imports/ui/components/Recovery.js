5f37812817bd8b44d0fff6b15253dd51ff099956
pbca26 <pbca26@gmail.com>
2018-06-20 13:36:03 +0300

    new skin seed recovery

--- imports/ui/components/Recovery.js
+++ imports/ui/components/Recovery.js
@@ -52,45 +52,46 @@ class Recovery extends React.Component {
 
   render() {
     return (
-      <div className="col-sm-12">
-        <div className="col-xlg-12 col-md-12 col-sm-12 col-xs-12">
-          <div className="row">
-            <h4 className="padding-bottom-15">Recovery passphrase</h4>
-            <div className="padding-bottom-10">
-            Provide your PIN number to unlock passphrase.
+      <div className="form recovery">
+        <div className="margin-top-45 padding-bottom-35 text-center">
+        Provide your PIN number to unlock passphrase.
+        </div>
+        <div className="margin-bottom-25">
+          <div className="edit">
+            <input
+              type="password"
+              className="form-control"
+              name="pin"
+              onChange={ this.updateInput }
+              placeholder={ translate('LOGIN.ENTER_6_DIGIT_PIN') }
+              value={ this.state.pin || '' } />
+          </div>
+          { this.state.wrongPin &&
+            <div className="error margin-top-15">
+              <i className="fa fa-warning"></i> { translate('LOGIN.WRONG_PIN') }
             </div>
-            <div className="margin-bottom-25">
-              <input
-                type="password"
-                className="form-control margin-top-20"
-                name="pin"
-                onChange={ this.updateInput }
-                placeholder={ translate('LOGIN.ENTER_6_DIGIT_PIN') }
-                value={ this.state.pin || '' } />
-              { this.state.wrongPin &&
-                <div className="error margin-top-15">
-                  <i className="fa fa-warning"></i> { translate('LOGIN.WRONG_PIN') }
-                </div>
-              }
-            </div>
-            <button
-              disabled={ !this.state.pin }
-              className="btn btn-lg btn-primary btn-block ladda-button"
-              onClick={ this.decodeSeed }>
-              <span className="ladda-label">
-              Show
-              </span>
-            </button>
-            { this.state.passphrase &&
-              <div className="margin-bottom-25 margin-top-70 decoded-seed">
-                <div className="margin-bottom-40 fs-16 seed-string">{ this.state.passphrase }</div>
-                <QRCode
-                  value={ this.state.passphrase }
-                  size={ 240 } />
-              </div>
-            }
+          }
+        </div>
+        <div
+          disabled={ !this.state.pin }
+          onClick={ this.decodeSeed }
+          className="group3 margin-top-40">
+          <div className="rectangle10copy"></div>
+          <div className="btn">Show</div>
+          <div className="group2">
+            <i className="fa fa-eye"></i>
           </div>
         </div>
+        { this.state.passphrase &&
+          <div className="margin-bottom-25 margin-top-50 decoded-seed">
+            <div className="seed-gen-box margin-bottom-30">{ this.state.passphrase }</div>
+            <div className="text-center">
+              <QRCode
+                value={ this.state.passphrase }
+                size={ 360 } />
+            </div>
+          </div>
+        }
       </div>
     );
   }
