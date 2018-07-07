e71c57cb0f820ba80bee7d1ea98a768893bf4c3e
pbca26 <pbca26@gmail.com>
2018-07-07 22:12:13 +0300

    btc ui, pub address check

--- imports/ui/components/SendCoin.js
+++ imports/ui/components/SendCoin.js
@@ -5,6 +5,7 @@ import translate from '../translate/translate';
 import {
   convertURIToImageData,
   getLocalStorageVar,
+  isAssetChain,  
 } from '../actions/utils';
 import { decryptkey } from '../actions/seedCrypt';
 import jsQR from 'jsqr';
@@ -22,6 +23,11 @@ import {
   explorerList,
   isKomodoCoin,
 } from 'agama-wallet-lib/src/coin-helpers';
+import {
+  addressVersionCheck,
+} from 'agama-wallet-lib/src/keys';
+import electrumServers from '../actions/electrumServers';
+import electrumJSNetworks from 'agama-wallet-lib/src/bitcoinjs-networks';
 
 class SendCoin extends React.Component {
   constructor() {
@@ -33,6 +39,7 @@ class SendCoin extends React.Component {
       sendResult: {},
       spvVerificationWarning: false,
       spvPreflightSendInProgress: false,
+      spvPreflightResult: null,
       validNan: false,
       validTooMuch: false,
       validIncorrectAddress: false,
@@ -40,6 +47,8 @@ class SendCoin extends React.Component {
       wrongPin: false,
       pin: config.preload ? config.preload.pin : '',
       processing: false,
+      btcFee: 'halfHourFee',
+      fee: null,
     };
     this.defaultState = JSON.parse(JSON.stringify(this.state));
     this.changeSendCoinStep = this.changeSendCoinStep.bind(this);
@@ -154,6 +163,8 @@ class SendCoin extends React.Component {
       validIncorrectAddress: false,
       qrScanError: false,
       wrongPin: false,
+      wrongAddress: false,
+      fee: null,
     });
   }
 
@@ -168,8 +179,8 @@ class SendCoin extends React.Component {
       _isFailed = true;
     }
 
-    if (this.state.sendTo.length < 34 ||
-        this.state.sendTo.length > 36) {
+    if (!addressVersionCheck(electrumJSNetworks[isAssetChain(this.props.coin) ? 'kmd' : this.props.coin], this.state.sendTo) ||
+        addressVersionCheck(electrumJSNetworks[isAssetChain(this.props.coin) ? 'kmd' : this.props.coin], this.state.sendTo) === 'Invalid pub address') {
       validIncorrectAddress = true;
       _isFailed = true;
     }
@@ -196,6 +207,14 @@ class SendCoin extends React.Component {
   }
 
   changeSendCoinStep(step, back) {
+    if (step === 0 &&
+        this.props.coin === 'btc') {
+      this.props.getBtcFees();
+      this.setState({
+        btcFee: 'halfHourFee',
+      });
+    }
+
     if (back) {
       this.setState({
         sendCurrentStep: step,
@@ -204,6 +223,7 @@ class SendCoin extends React.Component {
         validNan: false,
         pin: '',
         wrongPin: false,
+        fee: null,
       });
     } else {
       if (!this.validate()) {
@@ -223,18 +243,23 @@ class SendCoin extends React.Component {
               this.state.sendTo,
               Math.abs(toSats(this.state.sendAmount)),
               true,
-              false
+              false,
+              this.props.coin === 'btc' ? this.props.btcFees[this.state.btcFee] : null
             )
             .then((sendPreflight) => {
+              console.warn('sendPreflight', sendPreflight);
+
               if (sendPreflight &&
                   sendPreflight.msg === 'success') {
                 this.setState({
                   spvVerificationWarning: !sendPreflight.result.utxoVerified,
                   spvPreflightSendInProgress: false,
+                  spvPreflightResult: sendPreflight,
                 });
               } else {
                 this.setState({
                   spvPreflightSendInProgress: false,
+                  spvPreflightResult: sendPreflight,
                 });
               }
             });
@@ -254,7 +279,8 @@ class SendCoin extends React.Component {
               this.state.sendTo,
               Math.abs(toSats(this.state.sendAmount)),
               null,
-              true
+              true,
+              this.props.coin === 'btc' ? this.props.btcFees[this.state.btcFee] : null
             )
             .then((res) => {
               devlog('sendtx result');
@@ -329,16 +355,42 @@ class SendCoin extends React.Component {
               </div>
             }
           </div>
+          { this.props.coin === 'btc' &&
+            this.props.btcFees &&
+            this.props.btcFee !== 'error' &&
+            <div className="edit">
+              <label className="control-label">{ translate('SEND.FEE') }</label>
+              <select
+                className="margin-top-15 margin-bottom-10"
+                name="btcFee"
+                value={ this.state.btcFee }
+                onChange={ (event) => this.updateInput(event) }>
+                <option value="fastestFee">{ translate('SEND.BTC_FEE_FAST') }</option>
+                <option value="halfHourFee">{ translate('SEND.BTC_FEE_AVG') }</option>
+                <option value="hourFee">{ translate('SEND.BTC_FEE_SLOW') }</option>
+              </select>
+            </div>
+          }
           { this.state.qrScanError &&
             <div className="error margin-top-15 text-center">
               <i className="fa fa-warning"></i> { translate('SEND.QR_SCAN_ERR') }
             </div>
           }
+          { this.props.coin === 'btc' &&
+            !this.props.btcFees &&
+            <div className="margin-top-15 text-center">{ translate('SEND.BTC_FEES_FETCHING') }</div>
+          }
+          { this.props.coin === 'btc' &&
+            this.props.btcFees &&
+            this.props.btcFees === 'error' &&
+            <div className="error margin-top-15 text-center">{ translate('SEND.BTC_FEES_FETCHING_FAILED') }</div>
+          }
           <div>
             <div
               disabled={
                 !this.state.sendTo ||
-                !this.state.sendAmount
+                !this.state.sendAmount ||
+                (this.props.coin === 'btc' && (!this.props.btcFees || this.props.btcFees === 'error'))
               }
               onClick={ () => this.changeSendCoinStep(1) }
               className="group3 margin-top-50">
@@ -399,6 +451,27 @@ class SendCoin extends React.Component {
                 { this.state.sendAmount } { this.props.coin.toUpperCase() }
                 </span>
               </div>
+              { this.state.spvPreflightResult &&
+                this.state.spvPreflightResult.msg === 'success' &&
+                <div>
+                  <div className="padding-top-15 edit">
+                    <strong>{ translate('SEND.FEE') }</strong>
+                  </div>
+                  <div className="edit">
+                    <span className="shade">
+                    { Number(fromSats(this.state.spvPreflightResult.result.fee)) } { this.props.coin.toUpperCase() }
+                    </span>
+                  </div>
+                  <div className="padding-top-15 edit">
+                    <strong>{ translate('SEND.TOTAL') }</strong>
+                  </div>
+                  <div className="edit">
+                    <span className="shade">
+                    { formatValue(Number(fromSats(this.state.spvPreflightResult.result.value)) + Number(fromSats(this.state.spvPreflightResult.result.fee))) } { this.props.coin.toUpperCase() }
+                    </span>
+                  </div>
+                </div>
+              }
             </div>
             { ((getLocalStorageVar('settings') && getLocalStorageVar('settings').requirePin) ||
               (config.preload && config.enablePinConfirm)) &&
