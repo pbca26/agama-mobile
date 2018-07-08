417dd7a369dfe2b6da907e27c718f31fc04db5f5
pbca26 <pbca26@gmail.com>
2018-07-08 13:29:02 +0300

    remove old tx decoder code, dev config example

---
+++ imports/ui/actions/dev.js
@@ -0,0 +1,31 @@
+export const config = {
+  dev: false,
+};
+
+export const devlog = (msg, data) => {
+  if (config.dev) {
+    if (data) {
+      console.warn(msg, data);
+    } else {
+      console.warn(msg);
+    }
+  }
+};
+
+/* dev data example, use it for testing purposes only!
+config {
+  dev: true,
+  preload: {
+    seed: 'xyz',
+    pin: '777777',
+    enablePinConfirm: true,
+    coins: 'kmd,chips',
+    activeCoin: 'chips',
+    send: {
+      amount: 0.0001,
+      address: 'xyz',
+    },
+    disableAutoLock: true,
+  },
+};
+*/
