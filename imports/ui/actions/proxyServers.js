b27cabe5b5901081fbf25081d8d51a9fddfa2bf4
pbca26 <pbca26@gmail.com>
2018-07-09 18:13:50 +0300

    use port 80 for proxies

--- imports/ui/actions/proxyServers.js
+++ imports/ui/actions/proxyServers.js
@@ -2,8 +2,8 @@
 
 export const proxyServers = [{
   ip: '94.130.225.86',
-  port: 9999,
+  port: 80,
 }, {
   ip: '94.130.98.74',
-  port: 9999,
+  port: 80,
 }];
--- mobile-config.js
+++ mobile-config.js
@@ -43,6 +43,8 @@ App.launchScreens({
 // proxies
 App.accessRule('http://94.130.225.86');
 App.accessRule('http://94.130.98.74');
+App.accessRule('http://94.130.225.86:80');
+App.accessRule('http://94.130.98.74:80');
 // atomic
 App.accessRule('https://www.atomicexplorer.com');
 // coin explorers
