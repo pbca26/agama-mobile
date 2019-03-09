f3a56e753bcb4dc94ea8ec394962cbc3c665c21e
pbca26 <pbca26@gmail.com>
2019-03-09 11:45:18 +0300

    action utils shuffle array

--- imports/ui/actions/utils.js
+++ imports/ui/actions/utils.js
@@ -139,3 +139,14 @@ export const joinPropsUrl = (obj) => {
 
   return out.join('&');
 };
+
+// ref: https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+export const shuffleArray = (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+
+  return a;
+};
