970839d4bab09402c32ef991c3f31376c5338ae2
pbca26 <pbca26@gmail.com>
2019-04-08 21:45:00 +0300

    utils move sortTransactions function to agama-wallet-lib

--- imports/ui/actions/utils.js
+++ imports/ui/actions/utils.js
@@ -101,34 +101,6 @@ export const removeDisabledSPVCoins = (coins) => {
   return coins;
 };
 
-export const sortTransactions = (transactions, sortBy) => {
-  return transactions.sort((b, a) => {
-    if (a[sortBy ? sortBy : 'height'] < b[sortBy ? sortBy : 'height'] &&
-        a[sortBy ? sortBy : 'height'] &&
-        b[sortBy ? sortBy : 'height']) {
-      return -1;
-    }
-
-    if (a[sortBy ? sortBy : 'height'] > b[sortBy ? sortBy : 'height'] &&
-        a[sortBy ? sortBy : 'height'] &&
-        b[sortBy ? sortBy : 'height']) {
-      return 1;
-    }
-
-    if (!a[sortBy ? sortBy : 'height'] &&
-        b[sortBy ? sortBy : 'height']) {
-      return 1;
-    }
-
-    if (!b[sortBy ? sortBy : 'height'] &&
-        a[sortBy ? sortBy : 'height']) {
-      return -1;
-    }
-
-    return 0;
-  });
-}
-
 // single level
 export const joinPropsUrl = (obj) => {
   let out = [];
