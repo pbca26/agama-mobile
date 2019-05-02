e797ae234295d941d318ceb6438e759af044973d
pbca26 <pbca26@gmail.com>
2019-05-02 19:20:49 +0300

    actions getServerVersion cached server protocol versions obj

--- imports/ui/actions/serverVersion.js
+++ imports/ui/actions/serverVersion.js
@@ -1,33 +1,56 @@
 import { devlog } from './dev';
 import { Promise } from 'meteor/promise';
+import {
+  getLocalStorageVar,
+  setLocalStorageVar,
+} from './utils';
 
 const getServerVersion = async function (proxyServer, port, ip, proto) {
+  let protocolVersion = getLocalStorageVar('protocolVersion');
+  
   return new Promise((resolve, reject) => {
-    const params = {
-      port,
-      ip,
-      proto,
-    };
-    devlog('req', {
-      method: 'GET',
-      url: `http://${proxyServer.ip}:${proxyServer.port}/api/server/version`,
-      params,
-    });
+    if (protocolVersion[`${ip}:${port}:${proto}`] &&
+        protocolVersion[`${ip}:${port}:${proto}`] === 1.4) {
+      resolve(true);
+    } else {
+      const params = {
+        port,
+        ip,
+        proto,
+      };
+      devlog('req', {
+        method: 'GET',
+        url: `http://${proxyServer.ip}:${proxyServer.port}/api/server/version`,
+        params,
+      });
 
-    HTTP.call(
-      'GET',
-      `http://${proxyServer.ip}:${proxyServer.port}/api/server/version`, {
-      params,
-    }, (error, result) => {
-      result = JSON.parse(result.content);
+      HTTP.call(
+        'GET',
+        `http://${proxyServer.ip}:${proxyServer.port}/api/server/version`, {
+        params,
+      }, (error, result) => {
+        result = JSON.parse(result.content);
 
-      if (result.msg === 'error') {
-        return 'error';
-      } else {
-        console.warn(result.result);
-        resolve(true);
-      }
-    });
+        if (result.msg === 'error') {
+          return 'error';
+        } else {
+          devlog('getServerVersion', result.result);
+
+          if (result.result &&
+              typeof result.result === 'object' &&
+              result.result.length === 2 &&
+              result.result[0].indexOf('ElectrumX') > -1 &&
+              Number(result.result[1]) === 1.4) {
+            protocolVersion[`${ip}:${port}:${proto}`] = 1.4;
+            setLocalStorageVar('protocolVersion', protocolVersion);
+
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        }
+      });
+    }
   });
 }
 
